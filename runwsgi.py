4d724e71831685d6339f5cf16ca8b688b0d08bd9
Jittat Fakcharoenphol <jittat@fledged.(none)>
2009-09-21 00:17:14 +0700

    added wsgi server from cherrypy

---
+++ runwsgi.py
@@ -0,0 +1,26 @@
+"""
+run project with CherryPy wsgi server taken from
+http://www.eflorenzano.com/blog/post/hosting-django-site-pure-python/
+"""
+
+import wsgiserver
+import os
+import sys
+import django.core.handlers.wsgi
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
+
+if __name__ == "__main__":
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+    server = wsgiserver.CherryPyWSGIServer(
+        ('0.0.0.0', 8000),
+        django.core.handlers.wsgi.WSGIHandler(),
+        server_name='www.django.example',
+        numthreads = 20,
+    )
+
+    print 'started...'
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
