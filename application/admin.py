e5b074967124f347c70bdf87c9c647e2c7716116
Jittat Fakcharoenphol <jittat@gmail.com>
2009-11-18 20:39:17 +0700

    added search to django admin pages

--- application/admin.py
+++ application/admin.py
@@ -3,15 +3,31 @@ from models import Applicant, SubmissionInfo, PersonalInfo
 from models import Address, ApplicantAddress
 from models import Education, Major, GPExamDate
 
-class ApplicantAdmin(admin.ModelAdmin):
-    exclude = ('has_related_model',)
-
-admin.site.register(Applicant, ApplicantAdmin)
-
 admin.site.register(SubmissionInfo)
 admin.site.register(PersonalInfo)
 admin.site.register(Address)
-admin.site.register(ApplicantAddress)
 admin.site.register(GPExamDate)
 admin.site.register(Education)
 admin.site.register(Major)
+
+
+class ApplicantAddressAdmin(admin.ModelAdmin):
+    search_fields = ['applicant__email']
+
+admin.site.register(ApplicantAddress, ApplicantAddressAdmin)
+
+class PersonalInfoInline(admin.StackedInline):
+    model = PersonalInfo
+
+class ApplicantAdmin(admin.ModelAdmin):
+    exclude = ('has_related_model',)
+
+    list_display = ['full_name', 'email']
+    search_fields = ['email']
+
+    inlines = [
+        PersonalInfoInline,
+        ]
+
+admin.site.register(Applicant, ApplicantAdmin)
+
