92475174983a340e115d845d3f5c168c61812ee6
Jittat Fakcharoenphol <jittat@gmail.com>
2009-11-18 22:19:39 +0700

    added icons on ticket search

--- application/templatetags/adm_media.py
+++ application/templatetags/adm_media.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.conf import settings
 
 register = Library()
 
@@ -6,9 +7,15 @@ def media_url():
     """
     Returns the string contained in the setting MEDIA_URL.
     """
-    try:
-        from django.conf import settings
-    except ImportError:
-        return ''
     return settings.MEDIA_URL
 media_url = register.simple_tag(media_url)
+
+
+def passed_icon(is_passed=True):
+    if is_passed:
+        img_src="/image/Clear.png"
+    else:
+        img_src="/image/MinusRed.png"
+    return "%s%s" % (settings.MEDIA_URL, img_src)
+passed_icon = register.simple_tag(passed_icon)
+
