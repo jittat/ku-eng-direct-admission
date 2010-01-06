d1f20026d4d059492e42fa6ac9f3c0562423cbbc
Jittat Fakcharoenphol <jittat@glider.(none)>
2010-01-06 23:24:09 +0700

    major_pref export prefer online prefs, shows pref in online review

--- scripts/export_major_pref.py
+++ scripts/export_major_pref.py
@@ -16,13 +16,45 @@ from utils import get_submitted_applicant_dict
 applicants = get_submitted_applicant_dict({
         'preference': MajorPreference,
         'personal_info': PersonalInfo,
+        'submission_info': SubmissionInfo,
         })
 
 f = codecs.open(file_name, encoding="utf-8", mode="w")
-for applicant in applicants.itervalues():
+
+pref = {}
+
+SUBMISSION_RANK = {1: 2, # doc by mail
+                   2: 1, # online
+                   3: 3} # all by mail
+
+for applicantion_id in sorted(applicants.keys()):
+    applicant = applicants[applicantion_id]
+    if not applicant.submission_info.doc_reviewed_complete:
+        continue
     majors_str = ",".join([str(m) for m in  applicant.preference.majors])
-    print >> f, "%s,%s,%d,%s" % (
+    nat_id = applicant.personal_info.national_id
+    submission_method = applicant.doc_submission_method
+    output_str = "%s,%s,%d,%s" % (
         applicant.ticket_number(),
         applicant.personal_info.national_id,
         len(applicant.preference.majors),
         majors_str)
+
+    if nat_id in pref:
+        old_pref = pref[nat_id]
+
+        if SUBMISSION_RANK[submission_method] > SUBMISSION_RANK[old_pref[1]]:
+            print "Old - better: [%s/%d] > [%s/%d]" % (
+                old_pref[0], old_pref[1],
+                output_str, submission_method)
+            continue
+        else:
+            print "New - better: [%s/%d] > [%s/%d]" % (
+                output_str, submission_method,
+                old_pref[0], old_pref[1])
+
+    pref[nat_id] = (output_str,
+                    submission_method)
+
+for output, method in pref.itervalues():
+    print >> f, output
