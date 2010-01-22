718bc687e99234d860d49aaf64c3b8ed03c6c431
Jittat Fakcharoenphol <jittat@gmail.com>
2010-01-22 15:53:29 +0700

    shows choices of confirmed students

--- scripts/dump_choices.py
+++ scripts/dump_choices.py
@@ -5,7 +5,7 @@ from django_bootstrap import bootstrap
 bootstrap(__file__)
 
 from application.models import Applicant
-from confirmation.models import AdmissionMajorPreference
+from confirmation.models import AdmissionMajorPreference, AdmissionConfirmation
 
 def main():
     prefs = AdmissionMajorPreference.objects.all()
@@ -15,7 +15,24 @@ def main():
         pref_type = p.get_pref_type()
         counts[pref_type.ptype] += 1
 
+    print 'all:'
     print counts
+    
+    con_counts = dict([(i,0) for i in [1,2,3,4]])
+    confirmed = AdmissionConfirmation.objects.all()
+    for c in confirmed:
+        p = None
+        try:
+            if c.applicant.admission_major_preference != None:
+                p = c.applicant.admission_major_preference
+        except:
+            pass
+        if p!=None:
+            pref_type = p.get_pref_type()
+            con_counts[pref_type.ptype] += 1
+    
+    print 'confirmed:'
+    print con_counts
 
 if __name__ == '__main__':
     main()
