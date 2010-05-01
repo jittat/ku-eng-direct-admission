5b2c972921f2b532a9a2e81257d4ae8b2187de5f
Jittat Fakcharoenphol <jittat@gmail.com>
2010-05-01 10:50:34 +0700

    updates major pref for single major pref

--- application/views/update.py
+++ application/views/update.py
@@ -3,6 +3,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from commons.decorators import submitted_applicant_required
 from commons.decorators import within_submission_deadline
@@ -10,15 +11,54 @@ from commons.utils import submission_deadline_passed, redirect_to_deadline_error
 
 from application.views.form_views import prepare_major_form
 from application.forms.handlers import handle_major_form
+from application.forms.handlers import assign_major_pref_to_applicant
 from application.forms.handlers import handle_education_form
-from application.forms import EducationForm
-from application.models import Applicant, MajorPreference
+from application.forms import EducationForm, SingleMajorPreferenceForm
+from application.models import Applicant, MajorPreference, Major
 
 from commons.email import send_sub_method_change_notice_by_email
+
+def update_major_single_choice(request):
+    applicant = request.applicant
+
+    if request.method == 'POST':
+
+        if 'cancel' not in request.POST:
+            form = SingleMajorPreferenceForm(request.POST)
+            if form.is_valid():
+                assign_major_pref_to_applicant(applicant,
+                                               [form.cleaned_data['major'].number])
+                request.session['notice'] = 'การแก้ไขอันดับสาขาวิชาเรียบร้อย'
+                return HttpResponseRedirect(reverse('status-index'))
+        else:
+            request.session['notice'] = 'อันดับสาขาวิชาไม่ถูกแก้ไข'
+            return HttpResponseRedirect(reverse('status-index'))
+
+    else:
+        if applicant.has_major_preference():
+            pref = applicant.preference.majors
+            if len(pref)==0:
+                prev_major = None
+            else:
+                majors = dict([(int(m.number), m) for m in Major.get_all_majors()])
+                prev_major = majors[pref[0]]
+        form = SingleMajorPreferenceForm(initial={'major': prev_major.id})
+
+    # add step info
+    form_data = {}
+    form_data['step_name'] = 'แก้ไขอันดับสาขาวิชา'
+    form_data['can_log_out'] = True
+    form_data['form'] = form
+    return render_to_response('application/update/majors_single.html',
+                              form_data)
+    
 
 @within_submission_deadline
 @submitted_applicant_required
 def update_majors(request):
+    if settings.MAX_MAJOR_RANK == 1:
+        return update_major_single_choice(request)
+
     applicant = request.applicant
 
     if request.method == 'POST': 
