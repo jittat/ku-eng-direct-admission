1e9590f7633c275ef368840e9ab39e13d6d46560
Jittat Fakcharoenphol <jittat@gmail.com>
2011-02-16 08:15:22 +0700

    exports names with results

--- scripts/export_results_with_confirmation.py
+++ scripts/export_results_with_confirmation.py
@@ -1,7 +1,8 @@
 import sys
+import codecs
 
-if len(sys.argv)!=2:
-    print "Usage: export_results_with_confirmation.py [round_number]"
+if len(sys.argv)<2:
+    print "Usage: export_results_with_confirmation.py [round_number] (optional-filename)"
     quit()
 
 from django.conf import settings
@@ -14,6 +15,11 @@ from application.models import Applicant
 
 def main():
     round_number = int(sys.argv[1])
+    if len(sys.argv)>2:
+        filename = sys.argv[2]
+        fp = codecs.open(filename,"w",encoding="utf-8")
+    else:
+        fp = None
 
     results = (AdmissionResult.
                objects.
@@ -25,7 +31,11 @@ def main():
         app = r.applicant
         paid_total = sum([c.paid_amount for c in app.admission_confirmations.all()])
         confirmed = paid_total >= r.admitted_major.confirmation_amount
-        print "%s,%d,%s" % (app.national_id, int(r.admitted_major.number), str(confirmed))
+        if not fp:
+            print "%s,%d,%s" % (app.national_id, int(r.admitted_major.number), str(confirmed))
+        else:
+            print >>fp, u"%s,%s,%d,%s" % (app.national_id, app.full_name(), int(r.admitted_major.number), str(confirmed))
+            
 
 
 
